c1afc22a7221c844df5501d87ecc9c268f6a1265
Maarten <mljmol@hotmail.com>
2018-06-20 18:35:10 +0200

    first grid masking algorithm

---
+++ Grid masking.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 20 17:44:58 2018
+
+@author: mljmol
+"""
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+Path = r"C:\Users\mljmo\OneDrive\GIMA\Thesis\Data\joinedtracks"
+os.chdir(Path)
+
+def formGrid(df, gridPoints=10):
+    xMin, xMax, yMin, yMax = df["X"].min(), df["X"].max(), df["Y"].min(), df["Y"].max()
+    gridX = np.linspace(xMin, xMax, gridPoints)
+    gridY = np.linspace(yMin, yMax, gridPoints)
+    return gridX, gridY
+
+def findClosest(val, gridVals):
+    idx = np.argmin(abs(val-gridVals))
+    return gridVals[idx]
+    
+def snapToGrid(df, gridX, gridY):
+    X = df["X"].apply(lambda x: findClosest(x, gridX))
+    Y = df["Y"].apply(lambda y: findClosest(y, gridY))
+    df["X"] = X
+    df["Y"] = Y
+    return df
+
+
+def gridMask(df, gridPoints=10):
+    gridX, gridY = formGrid(df, gridPoints)
+    df = snapToGrid(df, gridX, gridY)
+    return df
+    
+
+df = pd.read_csv("joinedTracks.csv")
+
+track = df[df["track"]==301757]
+df = track.copy()
+df = gridMask(df, gridPoints=3)
+
+
+
+plt.subplot(2,1,1)
+plt.title("Original data")
+plt.scatter(track["X"], track["Y"])
+plt.subplot(2,1,2)
+plt.title("applied grid masking")
+plt.scatter(df["X"], df["Y"])
+track==df
+
+
+
+
+#plt.scatter(track["X"], track["Y"])
+#[track["X"], track["Y"]] == [df["X"],df["Y"]]
